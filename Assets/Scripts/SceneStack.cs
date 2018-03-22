b4915a94ea5d4a09b86f2172d4d40548174f37b3
Dave Hards <unstablesun.gmail.com>
2018-03-22 13:30:30 -0700

    new assets

--- Assets/Scripts/SceneStack.cs
+++ Assets/Scripts/SceneStack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneStack : MonoBehaviourSingletonPersistent<SceneStack> 
+public class SceneStack : Singleton<SceneStack> 
 {
 	public enum StackItemType
 	{
--- Assets/Scripts/Util/Singleton.cs
+++ Assets/Scripts/Util/Singleton.cs
@@ -1,77 +1,65 @@
+using System;
 using UnityEngine;
+using Trace = System.Diagnostics.Trace;
 
-/// <summary>
-/// Be aware this will not prevent a non singleton constructor
-///   such as `T myT = new T();`
-/// To prevent that, add `protected T () {}` to your singleton class.
-/// 
-/// As a note, this is made as MonoBehaviour because we need Coroutines.
-/// </summary>
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-	private static T _instance;
+    /// <summary>
+    /// A thread-safe singleton implementation for MonoBehaviors.
+    /// </summary>
+    public class Singleton<T> : MonoBehaviour where T : class
+    {
+        private static volatile object lockSync = new object();
+        private static T instance = null;
 
-	private static object _lock = new object();
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockSync)
+                    {
+                        if (instance == null)
+                        {
+                            Trace.TraceInformation(string.Format("Creating Singleton {0}", typeof(T).Name));
+                            instance = SingletonManager.GetSingletonContainer(typeof(T).Namespace).AddComponent(typeof(T)) as T;
+                        }
+                    }
+                }
 
-	public static T Instance
-	{
-		get
-		{
-			if (applicationIsQuitting) {
-				Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
-					"' already destroyed on application quit." +
-					" Won't create again - returning null.");
-				return null;
-			}
+                return instance;
+            }
+        }
 
-			lock(_lock)
-			{
-				if (_instance == null)
-				{
-					_instance = (T) FindObjectOfType(typeof(T));
+        public static void CreateInstance()
+        {
+            if (instance == null)
+            {
+                instance = Instance;
+            }
+        }
+    }
 
-					if ( FindObjectsOfType(typeof(T)).Length > 1 )
-					{
-						Debug.LogError("[Singleton] Something went really wrong " +
-							" - there should never be more than 1 singleton!" +
-							" Reopening the scene might fix it.");
-						return _instance;
-					}
+    /// <summary>
+    /// Maintains a copy of the game object holding singletons across scenes.
+    /// </summary>
+    internal class SingletonManager
+    {
+        private static GameObject gameObjectInstance = null;
 
-					if (_instance == null)
-					{
-						GameObject singleton = new GameObject();
-						_instance = singleton.AddComponent<T>();
-						singleton.name = "(singleton) "+ typeof(T).ToString();
+        public static GameObject GetSingletonContainer(string gameObjectName)
+        {
+            if (gameObjectInstance == null)
+            {
+                if (GameObject.Find(gameObjectName) != null)
+                {
+                    throw new InvalidOperationException("There's an old SingletonManager in the scene. Delete and save scene!");
+                }
 
-						DontDestroyOnLoad(singleton);
+                gameObjectInstance = new GameObject(gameObjectName);
+                gameObjectInstance.name = "SingletonContainer";
+                UnityEngine.Object.DontDestroyOnLoad(gameObjectInstance);
+            }
 
-						Debug.Log("[Singleton] An instance of " + typeof(T) + 
-							" is needed in the scene, so '" + singleton +
-							"' was created with DontDestroyOnLoad.");
-					} else {
-						Debug.Log("[Singleton] Using instance already created: " +
-							_instance.gameObject.name);
-					}
-				}
-
-				return _instance;
-			}
-		}
-	}
-
-	private static bool applicationIsQuitting = false;
-	/// <summary>
-	/// When Unity quits, it destroys objects in a random order.
-	/// In principle, a Singleton is only destroyed when application quits.
-	/// If any script calls Instance after it have been destroyed, 
-	///   it will create a buggy ghost object that will stay on the Editor scene
-	///   even after stopping playing the Application. Really bad!
-	/// So, this was made to be sure we're not creating that buggy ghost object.
-	/// </summary>
-	public void OnDestroy () {
-		//dhards this it not correct the app isn't quitting, dump code.
-		//Debug.Log ("OnDestroy?? Why!");
-		//applicationIsQuitting = true;
-	}
-}
+            return gameObjectInstance;
+        }
+    }
